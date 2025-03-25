e4553cba68dede97ae837ad2f00af7a72854780d
Leovi <lugoleovi@gmail.com>
2025-03-25 01:59:10 -0600

    fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Task 1: Display the current year in the footer's first paragraph
-    const currentYear = new Date().getFullYear(); // Get the current year
-    const yearElement = document.getElementById('currentyear'); // Find the element where the year should go
-    yearElement.textContent = currentYear; // Set the text content to the current year
-
-    // Task 2: Display the last modified date of the document in the second paragraph
-    const lastModified = document.lastModified; // Get the last modified date of the document
-    const lastModifiedElement = document.getElementById('lastModified'); // Find the element where the last modified date should go
-    lastModifiedElement.textContent = `Last modified: ${lastModified}`; // Display the last modified date
+    const currentYear = new Date().getFullYear(); 
+    const yearElement = document.getElementById('currentyear'); 
+    yearElement.textContent = currentYear; 
+   
+    const lastModified = document.lastModified; 
+    const lastModifiedElement = document.getElementById('lastModified'); 
+    lastModifiedElement.textContent = `Last modified: ${lastModified}`; 
 });
